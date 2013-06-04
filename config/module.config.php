85e2bc43590afcd7b0c4499ae33043f0efa5ac13
IgorDePaula <principe.borodin@gmail.com>
2013-06-04 12:10:07 -0300

    Inclusão das acl

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Diretoria;
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -8,6 +8,16 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 return array(
+      'acl' => array(
+        'Diretoria' => array(
+            'DIRETORIA' => array(
+                'Diretoria\Controller\Index:index',              
+            ),
+            'TI' => array(
+                'Diretoria\Controller\Index:index',              
+            ),
+        )
+    ),
     'router' => array(
         'routes' => array(
             // This defines the hostname route which forms the base
